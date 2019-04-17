6e770731d64cf8769cf98574b3a4d47215b195d6
JayCho <taylorenjeacho@gmail.com>
2019-04-17 15:49:11 +0900

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 	session_start();
 	header('Content-Type: text/html; charset=utf-8'); // utf-8인코딩
 
-	$db = new mysqli("localhost","jaycho","Upper$$546","ETI");
+	$db = new mysqli("localhost","jaycho","******","ETI");
 	$db->set_charset("utf8");
 
 	function mq($sql)
